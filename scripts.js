519c934ea5aa8f6f795268ac49cef8707df37066
Cosmin <110344174+Phillip0100@users.noreply.github.com>
2024-05-28 19:33:16 +0300

    Create function to take human choice

--- scripts.js
+++ scripts.js
@@ -4,3 +4,19 @@ function getComputerChoice(){
     let randomChoice = pick[Math.floor(Math.random() * pick.length)];    
     return randomChoice;
 }
+
+function getHumanChoice(){
+    let validChoice = true;
+    
+    while(validChoice = true){
+        let choice = prompt("Please enter your choice:", "Rock, paper or scissors").toLowerCase();
+        if(choice == "rock" || choice == "paper" || choice == "scissors"){
+            alert("Thanks!");
+            return choice;
+        }else{
+            validChoice = true;
+        }
+    }
+}
+
+
